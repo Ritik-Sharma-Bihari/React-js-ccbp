1eb82341dc2d49e43048ea4236491c395d6d353d
Ritiksharmaji <ritiksharma555598@gmail.com>
2024-05-27 05:12:22 +0530

    common mistake completed the FruitsCounter

--- COMPONENTS AND PROPES AND STATES-2/Common Mistakes/codingprecties_9/src/index.js
+++ COMPONENTS AND PROPES AND STATES-2/Common Mistakes/codingprecties_9/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import AppFruitCounter from './AppFruitCounter';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+
+    {/* this is for display the fruitscount  */}
+    <AppFruitCounter />
   </React.StrictMode>
 );
 
